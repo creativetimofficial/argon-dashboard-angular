8a67834b978bb36fba57e9bbfe7ee86bcaf8e819
Toma Rares <tomairares@gmail.com>
2020-10-02 11:05:49 +0300

    Refactor: Update datepicker style

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -23,16 +23,6 @@ export class DashboardComponent implements OnInit {
   public clicked: boolean = true;
   public clicked1: boolean = false;
 
-  fromDate: NgbDate;
-      toDate: NgbDate;
-      hoveredDate: NgbDate;
-
-
-      constructor(calendar: NgbCalendar) {
-         this.fromDate = calendar.getToday();
-         this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
-       }
-
   ngOnInit() {
 
     this.datasets = [
@@ -61,34 +51,6 @@ export class DashboardComponent implements OnInit {
 			data: chartExample1.data
 		});
   }
-
-
-  onDateSelection(date: NgbDate) {
-        console.log(this.fromDate)
-
-        if (!this.fromDate && !this.toDate) {
-          this.fromDate = date;
-        } else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
-          this.toDate = date;
-        } else {
-          this.toDate = null;
-          this.fromDate = date;
-        }
-        console.log(this.toDate)
-
-      }
-      isHovered(date: NgbDate) {
-        return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
-      }
-
-      isInside(date: NgbDate) {
-        return date.after(this.fromDate) && date.before(this.toDate);
-      }
-
-      isRange(date: NgbDate) {
-        return date.equals(this.fromDate) || date.equals(this.toDate) || this.isInside(date) || this.isHovered(date);
-      }
-
 
 
   public updateOptions() {
