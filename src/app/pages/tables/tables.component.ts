06fd9fa95ca3fd03d531d9781d0d304e1605d7bc
Henri <32584105+OctopusPie@users.noreply.github.com>
2022-10-13 12:46:00 +0200

    removed console log

--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -30,7 +30,6 @@ export class TablesComponent implements OnInit {
   }
 
   submit() {
-    console.log(this.mailList);
     this.http.post<any>('http://localhost:8080/api/rest/answer/quizz', { title: 'Mail list' }).subscribe(data => {
       this.mailList = data.id;
     });
