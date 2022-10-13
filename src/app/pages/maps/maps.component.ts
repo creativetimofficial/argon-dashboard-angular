3188fa59ead9d07b65f04b4065d77b3ba9a329d1
Henri <32584105+OctopusPie@users.noreply.github.com>
2022-10-13 14:57:52 +0200

    Questions from quizz post in the backend

--- src/app/pages/maps/maps.component.ts
+++ src/app/pages/maps/maps.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 declare const google: any;
 
 @Component({
@@ -7,8 +8,31 @@ declare const google: any;
   styleUrls: ['./maps.component.scss']
 })
 export class MapsComponent implements OnInit {
+  answers: Array<string>;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.answers = [];
+  }
+  onClick(string) {
+    let foo = false;
+    for (let i = 0; i < this.answers.length; i++) {
+      if (string === this.answers[i] || string.charAt(0) === this.answers[i].charAt(0)) {
+        foo = true;
+        this.answers.splice(i, 1);
+        i--;
+      }
+    }
+    if (foo === false) {
+      this.answers.push(string);
+    }
+  }
+
+  submit() {
+    console.log(this.answers);
+    this.http.post<any>('http://localhost:8080/api/rest/answer/quizz', { title: 'answers list' }).subscribe(data => {
+      this.answers = data.id;
+    });
+  }
 
   ngOnInit() {
 
