e8628858bb9a23c30bcb8f722d5e5d248b443640
Henri <32584105+OctopusPie@users.noreply.github.com>
2022-10-13 12:44:43 +0200

    Post email address to the back

--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {HttpClient} from '@angular/common/http';
+import {Element} from '@angular/compiler';
 
 @Component({
   selector: 'app-tables',
@@ -8,14 +9,32 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./tables.component.scss'],
 })
 export class TablesComponent implements OnInit {
-  postId;
+  mailList: Array<string>;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.mailList = [];
+  }
 
-  postMail(mailString) {
-    this.http.post<any>('http://localhost:8080/api/rest/answer/quizz', { title: 'Quizz Answers' }).subscribe(data => {
-      mailString = data.id;
+  onCheck(string) {
+    let foo = false;
+    for (let i = 0; i < this.mailList.length; i++) {
+      if (string === this.mailList[i]) {
+        foo = true;
+      }
+    }
+    if (foo === false) {
+      this.mailList.push(string);
+    }
+
+
+  }
+
+  submit() {
+    console.log(this.mailList);
+    this.http.post<any>('http://localhost:8080/api/rest/answer/quizz', { title: 'Mail list' }).subscribe(data => {
+      this.mailList = data.id;
     });
+    this.mailList = [];
   }
 
   ngOnInit() {
