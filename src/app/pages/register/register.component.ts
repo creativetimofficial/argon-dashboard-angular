c59a275251b73b75c5fe06ba635e6cc2e3b119ee
lassdouz <lassbnali@gmail.com>
2022-12-08 22:08:48 +0100

    add register alll

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    username: null,
+    email: null,
+    password: null
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
 
-  ngOnInit() {
+  constructor(private authService: AuthService,private router:Router) { }
+
+  ngOnInit(): void {
   }
+  onSubmit(): void {
+    const { username, email, password } = this.form;
 
+    this.authService.register(username, email, password).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+        console.log('ok');
+        this.router.navigate(['/login'] );   
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
 }
+
+  
+
+
